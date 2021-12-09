cf7a884da206271e0628f41bb6b08647cf9caada
Dayher Marchan <dayhermarchan@gmail.com>
2021-12-08 23:50:22 -0300

    commit de prueba

---
+++ Prueba/src/Prueba.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Prueba.c
+ Author      : Dayher Marchan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	char c = getchar();
+
+	putchar(c);
+	return EXIT_SUCCESS;
+}
